e67a14a5b47873e7a63545c968a9dec8f4709105
Anik Hossain <anik.wdev@gmail.com>
2025-08-16 15:32:59 +0600

    `Updated package.json and pnpm-lock.yaml files with new dependencies and versions`

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // Replace with your backend URL
+        target: 'https://project-management-fvz8.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
